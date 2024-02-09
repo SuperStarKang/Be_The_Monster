a7398e1b7cb7c1aac054f7680a79d88140d39770
이강민 <superstar@iyong-uui-MacBookPro.local>
2024-02-09 22:38:47 +0900

    2960.cpp

---
+++ 2960.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int n) {
+	for (int i=2; i*i <= n; i++) {
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main(void) {
+	int N,K;
+	cin>>N>>K;
+
+	int list[N+1];
+	int ans[N+1];
+	for (int i=0; i<=N; i++) {
+		list[i] = i;
+		ans[i] = 0;
+	}
+
+	int idx=0;
+	for (int i=2; i<=N; i++) {
+		for (int j=i; j<=N; j += i) {
+			if (list[j] == 0)
+				continue;
+			ans[idx++] = list[j];
+			list[j] = 0;
+		}
+	}
+	// for (int i=0; i<=N; i++) {
+	// 	cout<<list[i]<<' ';
+	// }
+	// cout<<endl;
+	// for (int i=0; i<=N; i++) {
+	// 	cout<<ans[i]<<' ';
+	// }
+	cout<<ans[K-1]<<endl;
+	return 0;
+}
