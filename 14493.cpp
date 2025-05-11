9fe6389f2ad234a274092e42c0197f27f9bff040
SuperStarKang <rkdalsfpdl@gmail.com>
2025-05-11 16:44:52 +0900

    과일노리

---
+++ 14493.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N,ans;
+vector<pair<int, int>> v;
+
+int main(void) {
+	cin >> N;
+
+	int a,b;
+	for (int i=0; i<N; i++) {
+		cin >> a >> b;
+		v.push_back(make_pair(a,b));
+	}
+	
+	int time=0,cnt=0;
+	while (cnt < N) {
+		if (!(time % (v[cnt].first + v[cnt].second) < v[cnt].second)) {
+			cnt++;
+		}
+		time += 1;
+	}
+	cout << time << '\n';
+	return 0;
+}
